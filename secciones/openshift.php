678220ecba346906ed0df976e6488f82ccf1ed8b
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:28:25 -0500

    Update openshift.php

--- secciones/openshift.php
+++ secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Dominando OpenShift: Orquestación de Contenedores",
+    "autor" => "Equipo de Ingenieros de Software",
+    "resumen" => "Una guía completa para comprender y utilizar OpenShift en la orquestación de contenedores, abordando desde conceptos básicos hasta estrategias avanzadas de implementación."
 ];
-?>
+
