c378bc46fa4e0799122b1f6924c2d117b16334e3
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:31:05 -0500

    Update video.php

--- secciones/video.php
+++ secciones/video.php
@@ -4,8 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El Poder del Video en la Era Digital",
+    "autor" => "Equipo de Producción Audiovisual",
+    "resumen" => "Un análisis sobre la relevancia y el impacto del contenido audiovisual en la era digital, explorando cómo el video ha revolucionado la comunicación, el entretenimiento y la educación en línea."
 ];
-?>
