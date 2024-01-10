0fea2774012401deb5accba2f0aeef353f3c7a73
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:27:38 -0500

    Update nacional.php

--- secciones/nacional.php
+++ secciones/nacional.php
@@ -4,8 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "La Era Moderna: Historia Nacional del Siglo XX",
+    "autor" => "Dr. Juan Martínez",
+    "resumen" => "Un análisis detallado de los eventos y transformaciones políticas, sociales y culturales que marcaron la historia nacional durante el siglo XX."
 ];
-?>
