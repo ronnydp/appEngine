988f4c2f2d3a7ae2e6bcb00c1baef45842b3dd85
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:18:34 -0500

    Update economia.php

--- secciones/economia.php
+++ secciones/economia.php
@@ -4,8 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Crisis Financieras: Lecciones del Pasado",
+    "autor" => "Economista Dr. Carlos Ramírez",
+    "resumen" => "Un estudio detallado de diversas crisis financieras a lo largo de la historia, analizando causas, impacto y lecciones aprendidas para prevenir futuras crisis."
 ];
-?>
