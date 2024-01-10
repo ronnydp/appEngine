733311198cda8bae0a1fc7926b6167ef65c447b1
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:20:42 -0500

    Update git.php

--- secciones/git.php
+++ secciones/git.php
@@ -4,8 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Dominando Git y GitHub",
+    "autor" => "Expertos en Desarrollo de Software",
+    "resumen" => "Una guía completa que abarca desde los conceptos básicos hasta las técnicas avanzadas de Git y GitHub, facilitando el control de versiones en proyectos de desarrollo de software."
 ];
-?>
