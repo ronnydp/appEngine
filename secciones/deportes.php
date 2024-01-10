74e30d896aa77748327fce90103052975bb757e8
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:18:00 -0500

    Update deportes.php

--- secciones/deportes.php
+++ secciones/deportes.php
@@ -4,8 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $deportes = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Estrategias Ganadoras: El Secreto del Éxito en el Deporte",
+    "autor" => "Entrenador Roberto González",
+    "resumen" => "Un análisis detallado de las estrategias y tácticas utilizadas por equipos y atletas exitosos en diferentes disciplinas deportivas."
 ];
-?>
