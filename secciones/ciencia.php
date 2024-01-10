e62b361cbd2dcc398cab46f2e72eb7725576f3a3
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:16:28 -0500

    Update ciencia.php

--- secciones/ciencia.php
+++ secciones/ciencia.php
@@ -4,8 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El Bosón de Higgs: La Partícula de la Creación",
+    "autor" => "Dr. María Pérez",
+    "resumen" => "Un estudio exhaustivo sobre el descubrimiento del Bosón de Higgs y su impacto en la comprensión de la física de partículas."
 ];
-?>
