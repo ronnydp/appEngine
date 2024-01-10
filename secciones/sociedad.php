6bf3fcf847458d7e9a9bc824ff63fffdfabc2767
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:30:06 -0500

    Update sociedad.php

--- secciones/sociedad.php
+++ secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Desafíos Actuales en la Sociedad Digital",
+    "autor" => "Equipo de Sociólogos",
+    "resumen" => "Un análisis profundo de los desafíos que enfrenta la sociedad en la era digital, desde la brecha digital hasta la privacidad en línea, explorando cómo estos aspectos impactan en la vida cotidiana y las relaciones sociales."
 ];
-?>
+
