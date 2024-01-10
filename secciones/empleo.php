6c0505207fc0a3b983fa045024c2e989a467d1ae
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:19:02 -0500

    Update empleo.php

--- secciones/empleo.php
+++ secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Navegando el Mercado Laboral Digital",
+    "autor" => "Especialistas en Recursos Humanos",
+    "resumen" => "Una guía completa para la búsqueda de empleo en la era digital, que abarca estrategias en línea, redes profesionales y consejos para destacar en el mercado laboral actual."
 ];
-?>
+
