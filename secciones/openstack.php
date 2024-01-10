a286443814657183ad5e8c2b5984d48a665d4999
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:28:45 -0500

    Update openstack.php

--- secciones/openstack.php
+++ secciones/openstack.php
@@ -4,8 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Desplegando Infraestructura en la Nube con OpenStack",
+    "autor" => "Equipo de Ingenieros Cloud",
+    "resumen" => "Una guía detallada para comprender y utilizar OpenStack en la implementación de infraestructura en la nube, cubriendo desde la instalación inicial hasta la administración avanzada de recursos."
 ];
-?>
