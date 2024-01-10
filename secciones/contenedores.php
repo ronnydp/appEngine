5baf190bc77648ba5fda6e960b80d941d68dd6cc
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:16:48 -0500

    Update contenedores.php

--- secciones/contenedores.php
+++ secciones/contenedores.php
@@ -4,8 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Optimización en la Gestión de Contenedores",
+    "autor" => "Ing. Carlos Fernández",
+    "resumen" => "Un análisis exhaustivo de las estrategias y tecnologías para optimizar la gestión de contenedores en el transporte marítimo y la logística global."
 ];
-?>
