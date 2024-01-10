bbdb083c8a36f13c97a4a3e9236993d803e5123d
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:17:35 -0500

    Update cultura.php

--- secciones/cultura.php
+++ secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El Arte a través de los Siglos",
+    "autor" => "Prof. Sofia Rodríguez",
+    "resumen" => "Un recorrido por la historia del arte desde las antiguas civilizaciones hasta el arte contemporáneo, explorando estilos, movimientos y artistas destacados."
 ];
-?>
+
