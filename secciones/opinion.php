72f3adbc2b4502f194083e62c7bee2aeaf9885ee
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:29:33 -0500

    Update opinion.php

--- secciones/opinion.php
+++ secciones/opinion.php
@@ -4,8 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "La Educación: Pilar Fundamental de la Sociedad",
+    "autor" => "Anónimo",
+    "resumen" => "La educación es el motor que impulsa el progreso de una sociedad. Al invertir en educación, se invierte en el futuro, empoderando a las generaciones venideras con conocimiento y habilidades para enfrentar los desafíos y contribuir al desarrollo sostenible."
 ];
-?>
