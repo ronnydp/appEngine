05bb3f438d9a5664a437c8d708eedcf6700a8eef
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:19:49 -0500

    Update gente.php

--- secciones/gente.php
+++ secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El Camino hacia el Bienestar Interior",
+    "autor" => "Psicólogo Dr. Javier Pérez",
+    "resumen" => "Un libro que explora estrategias psicológicas y herramientas para mejorar el bienestar emocional y mental en la vida diaria."
 ];
-?>
+
