bd90cd91665c6f22129ada3570da994cf08f2250
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:19:26 -0500

    Update formacion.php

--- secciones/formacion.php
+++ secciones/formacion.php
@@ -4,8 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "Gabinete de comunicación",
-"resumen" => "XXX",
+    "titulo" => "Aprendizaje Creativo: Innovación en la Educación",
+    "autor" => "Equipo de Expertos en Educación",
+    "resumen" => "Un libro que explora métodos innovadores de enseñanza y aprendizaje para estimular la creatividad y el pensamiento crítico en el aula."
 ];
-?>
