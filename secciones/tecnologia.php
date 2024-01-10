c554bad9d171f271586c6a75d8aba287149a7f53
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:30:24 -0500

    Update tecnologia.php

--- secciones/tecnologia.php
+++ secciones/tecnologia.php
@@ -4,8 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Innovación Tecnológica: Impulsando el Futuro",
+    "autor" => "Equipo de Expertos en Tecnología",
+    "resumen" => "Un análisis exhaustivo sobre las tendencias tecnológicas emergentes y su impacto en la sociedad, explorando desde inteligencia artificial hasta blockchain y su potencial para transformar industrias y mejorar la vida diaria."
 ];
-?>
