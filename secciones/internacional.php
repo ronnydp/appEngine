089bace4790cdd86c277ac4d97c90beddbaf75b4
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:09:24 -0500

    Update internacional.php

--- secciones/internacional.php
+++ secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Cumbre Global de Innovación",
+"autor" => "Organización Mundial de Innovación",
+"resumen" => "La cumbre reunirá a líderes de todo el mundo para discutir avances tecnológicos y su impacto en la sociedad global.",
 ];
 ?>
