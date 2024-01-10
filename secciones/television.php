5adc1e005b49a04ee4513727bd63ad4736e95457
Ronny <71031367+ronnydp@users.noreply.github.com>
2024-01-09 22:30:44 -0500

    Update television.php

--- secciones/television.php
+++ secciones/television.php
@@ -4,8 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "La Evolución de la Televisión: Pasado, Presente y Futuro",
+    "autor" => "Equipo de Investigación en Medios",
+    "resumen" => "Un recorrido por la historia de la televisión, desde sus inicios hasta las últimas innovaciones tecnológicas y cambios en los hábitos de consumo, explorando cómo la televisión ha moldeado y sigue moldeando nuestra cultura y entretenimiento."
 ];
-?>
